b7e5c23f4b067c70e2eb07d66412b325be0ba0a9
bumwhale333c <bumwhale333c@gmail.com>
2024-09-03 19:46:43 +0900

    launch_fix

--- sensor/erp42_communication/launch/erp42.launch.py
+++ sensor/erp42_communication/launch/erp42.launch.py
@@ -1,22 +1,23 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, TextSubstitution
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 def generate_launch_description():
-    erp_port_arg = DeclareLaunchArgument("erp_port", default_value=TextSubstitution(text="/dev/ttyUSB2"))
-
-    erp42_serial_node = Node(
-        package='erp42_communication',
-        executable='erp42_serial',
-        name='erp42_serial',
-        output='screen',
-        parameters=[{
-            "erp_port" : LaunchConfiguration('erp_port'),
-        }]
-    )
-
     return LaunchDescription([
-        erp_port_arg,
-        erp42_serial_node
+        DeclareLaunchArgument(
+            'erp_port',
+            default_value='/dev/ttyUSB2',
+            description='Serial port for ERP42'
+        ),
+
+        Node(
+            package='erp42_communication',
+            executable='erp42_serial',
+            name='erp42_serial',
+            output='screen',
+            parameters=[{
+                'erp_port': LaunchConfiguration('erp_port')
+            }]
+        ),
     ])
